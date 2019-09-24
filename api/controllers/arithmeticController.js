63b27d480313dcc13f9799bb9453e7f7442a2a08
lchemwolo <kimaiyoc@gmail.com>
2019-09-24 16:58:02 +0300

    commented correctly

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    #use +operator to coerce variables to numbers to avoid string concatenation
+    //use +operator to coerce variables to numbers to avoid string concatenation
     'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
